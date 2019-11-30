be274d7dee4c1922ebfe48328ef54681082e62c7
logan <807654909@qq.com>
2019-11-30 11:05:34 +0800

    none

--- src/h1/MTWriter.java
+++ src/h1/MTWriter.java
@@ -6,36 +6,42 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 public class MTWriter {
     private  byte[]data;
-    private int threadnum=2;  //
-
-    public void write() {
-        data = Producer.producer();
-        //将数据写入文件
-        try{
-        File file =new File("FileWriterWay.bin");
-        //if file doesnt exists, then create it
-        if(!file.exists()){
-            file.createNewFile();
-        }
-        //true = append file
-        FileWriter fileWritter = new FileWriter(file.getName(),true);
-        BufferedWriter bufferWritter = new BufferedWriter(fileWritter);
-        for(int i=0;i<data.length;i++) {
-            bufferWritter.write(data[i]);
-        }
-        bufferWritter.close();
-        System.out.println("Done");
-
-    }catch(IOException e){
-        e.printStackTrace();
+    private int num=0;
+    public  MTWriter()  //构造函数
+    {
+        data=Producer.producer();
     }
-}
+//    public void write() {
+//        data = Producer.producer();
+//        //将数据写入文件
+//        try{
+//        File file =new File("FileWriterWay.bin");
+//        //if file doesnt exists, then create it
+//        if(!file.exists()){
+//            file.createNewFile();
+//        }
+//        //true = append file
+//        FileWriter fileWritter = new FileWriter(file.getName(),true);
+//        BufferedWriter bufferWritter = new BufferedWriter(fileWritter);
+//        for(int i=0;i<data.length;i++) {
+//            bufferWritter.write(data[i]);
+//        }
+//        bufferWritter.close();
+//        System.out.println("Done");
+//
+//    }catch(IOException e){
+//        e.printStackTrace();
+//    }
+//}
 
 public void fliewriter(int threadnum)
 {
-
+    num=num+1;
+    int increasing=data.length/threadnum;
+    int leftindex=(num-1)*increasing;
+    int rigthindex=num*increasing-1;
     try{
-        File file =new File("FileWriterWay.bin");
+        File file =new File("threadnum"+threadnum+"FileWriterWay.txt");
         //if file doesnt exists, then create it
         if(!file.exists()){
             file.createNewFile();
@@ -43,11 +49,11 @@ public void fliewriter(int threadnum)
         //true = append file
         FileWriter fileWritter = new FileWriter(file.getName(),true);
         BufferedWriter bufferWritter = new BufferedWriter(fileWritter);
-        for(int i=0;i<data.length;i++) {
-            bufferWritter.write(data[i]);
+        for(int i=leftindex;i<=rigthindex;i++) {
+            bufferWritter.write((int)data[i]);
         }
         bufferWritter.close();
-        System.out.println("Done");
+        System.out.println("threadnum"+threadnum+"FileWriter Done");
 
     }catch(IOException e){
         e.printStackTrace();
@@ -55,12 +61,9 @@ public void fliewriter(int threadnum)
 
 
 }
-public static void main(String[] args)
+public void  setnum()
 {
-
-
-
-
+    num=0;
 }
 
 }
--- src/h1/main.java
+++ src/h1/main.java
@@ -8,18 +8,14 @@ import java.util.concurrent.Executors;
 public class main {
     public  static  void  main(String[] args)
     {
-
         MTWriter mtWriter =new MTWriter();
-        int[] threadnums={1,2,4,8,16,32};
-        int leftindex=0;
-        int datalength=2014*512*256;;
+        int[] threadnums={4};
         ExecutorService threadPool = Executors.newFixedThreadPool(32);
         for(int i=0;i<threadnums.length;i++)
         {
             int threadnum=threadnums[i];
             for(int j=0;j<threadnum;j++)
             {
-                int increasing=datalength/threadnum;
                 threadPool.execute(new Runnable() {
                     @Override
                     public void run() {
@@ -27,7 +23,6 @@ public class main {
                     }
                 });
 
-                leftindex=leftindex+increasing+1;
 
 //                rigthindex=2014*512*256/threadnum;
 //                new Thread(new Runnable() {
@@ -40,19 +35,20 @@ public class main {
 //                }).start();
 
             }
-
-
+            mtWriter.setnum();
         }
-        int leftindex=
-
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-
-                mtWriter.fliewriter();
-
-            }
-        }).start();
+        threadPool.shutdown(); // 设置线程池的状态为SHUTDOWN，然后中断所有没有正在执行任务的线程
+        threadPool.shutdownNow();
+//        int leftindex=
+//
+//        new Thread(new Runnable() {
+//            @Override
+//            public void run() {
+//
+//                mtWriter.fliewriter();
+//
+//            }
+//        }).start();
 
 //        MTWriter mtWriter=new MTWriter();
 //        mtWriter.write();
